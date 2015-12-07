e03aa695f8568edd9c0b6598b9347e20c96cf801
Krisztian Gyuris <kgyuris@cognex.com>
2015-12-07 10:28:27 +0100

    Initial commit of source

---
+++ ComputeWithMetal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComputeWithMetal
+//
+//  Created by Krisztian Gyuris on 07/12/15.
+//  Copyright © 2015 Krisztian Gyuris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
